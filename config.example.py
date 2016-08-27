7f36a4676009c30787cfbee2f7717fccfd717218
Sebastian Malek <sebastian.malek.ets@gmail.com>
2016-08-27 20:42:58 +0200

    meh

--- config.example.py
+++ config.example.py
@@ -1,11 +1,7 @@
-# the port WebAPI will listen on
 PORT = 8888
 
-# token for the Discord client
 DISCORD_TOKEN = ''
 
-# repositories/channels configuration
-# 'notify these channels if we receive a request from this repository'
 REPOSITORIES = {
     'sebastian-malek/devbot': [
         00000000000000000
